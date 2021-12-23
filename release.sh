65caf7b404f074d06ed25de97f7748e4ccf250ac
Mohamed Meabed <mo.meabed@gmail.com>
2021-12-23 18:28:48 +0400

    chose: update release

--- release.sh
+++ release.sh
@@ -9,4 +9,5 @@ npm version patch
 git push
 git push --tags
 cp package.json README.md LICENSE.md yarn.lock ./dist/
-npm publish ./dist
+grep -v '"prepare":' ./dist/package.json > ./dist/temp-package.json && mv -f ./dist/temp-package.json ./dist/package.json
+npm publish ./dist --ignore-scripts
