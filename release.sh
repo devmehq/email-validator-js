acd2eaaf1d50cd6e51f5c63ef27e30ce8ab0886b
Mohamed Meabed <mo.meabed@gmail.com>
2022-01-27 12:46:50 -0500

    chore: updates

--- release.sh
+++ release.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -e
 my_dir="$(dirname "$0")"
+version=${1:-patch}
 yarn test
 yarn build
 git add .
 git commit -am "chose: update release" || true
-npm version patch
-git push
-git push --tags
+npm version $version --message "chose: update release" || true
+git push || true
+git push --tags || true
 cp package.json README.md LICENSE.md CHANGELOG.md yarn.lock ./dist/
 grep -v '"prepare":' ./dist/package.json > ./dist/temp-package.json && mv -f ./dist/temp-package.json ./dist/package.json
 npm publish ./dist --ignore-scripts --access public
