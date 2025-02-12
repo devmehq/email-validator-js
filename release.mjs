21713922ea49210badb108c0e2dad9e8ca474b6e
Mohamed Meabed <mo.meabed@gmail.com>
2025-02-12 12:51:32 -0500

    chore: update release

--- release.mjs
+++ release.mjs
@@ -1,13 +1,20 @@
-import release from 'release-it'
+import release from 'release-it';
 
-const branch = process.env.BRANCH || process.env.CI_REF_NAME || ''
-const branchSlug = branch.replace(/\//g, '-')
-const branchPrefix = branch.split('/')[0]
+const branch = process.env.BRANCH || process.env.CI_REF_NAME || '';
+const branchSlug = branch.replace(/\//g, '-');
+const branchPrefix = branch.split('/')[0];
 
+/**
+ * @type {import('release-it').Config}
+ */
 const config = {
   isPreRelease: branch !== 'master',
   preRelease: branch !== 'master',
   preReleaseId: branch === 'master' ? '' : branch,
+  npm: {
+    skipChecks: true,
+    publishArgs: ['--access', 'public', '--registry', 'https://registry.npmjs.org/'],
+  },
   plugins: {
     '@release-it/conventional-changelog': {
       preset: {
@@ -23,26 +30,20 @@ const config = {
       },
     },
   },
-}
-
-console.debug('config', config)
+};
 
 release(config).then((output) => {
-  console.debug('output', output)
+  console.debug('output', output);
 
-  const { version: nextVersion, latestVersion, name, changelog } = output || {}
+  const { version: nextVersion, latestVersion, name, changelog } = output || {};
 
-  console.info(
-    `Last release is ${latestVersion} and next release is ${nextVersion}`
-  )
+  console.info(`Last release is ${latestVersion} and next release is ${nextVersion}`);
   if (latestVersion === nextVersion) {
-    console.info(
-      `No release is needed, last release is ${latestVersion} and next release is ${nextVersion}`
-    )
-    return
+    console.info(`No release is needed, last release is ${latestVersion} and next release is ${nextVersion}`);
+    return;
   }
 
   if (!nextVersion) {
-    console.info(`No release is needed - no next version`)
+    console.info(`No release is needed - no next version`);
   }
-})
+});
