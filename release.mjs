43871128025590f43a55c1d558ab163ab79f7060
Mohamed Meabed <mo.meabed@gmail.com>
2025-02-12 12:58:36 -0500

    chore: update release

--- release.mjs
+++ release.mjs
@@ -11,9 +11,6 @@ const config = {
   isPreRelease: branch !== 'master',
   preRelease: branch !== 'master',
   preReleaseId: branch === 'master' ? '' : branch,
-  npm: {
-    skipChecks: true,
-  },
   plugins: {
     '@release-it/conventional-changelog': {
       preset: {
